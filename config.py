f46675e65debbcba5f4fe52a5db73484b20f0267
Aluriak <lucas.bourneuf@laposte.net>
2016-12-06 00:14:22 +0100

    Configuration: new API based on methods code names + pretty printing

--- config.py
+++ config.py
@@ -4,9 +4,11 @@
 
 
 import random
+import functools
 from functools import partial
 from collections import namedtuple
 
+import utils
 import mutator
 import scoring
 import creation
@@ -25,16 +27,11 @@ class Configuration:
     def __init__(self, score:callable=None, select:callable=None,
                  mutate:callable=None, reproduce:callable=None,
                  create:callable=None):
-        if score is None:
-            score = scoring.functions()
-        if select is None:
-            select = selection.functions()
-        if mutate is None:
-            mutate = mutator.functions()
-        if reproduce is None:
-            reproduce = reproduction.functions()
-        if create is None:
-            create = creation.functions()
+        score = score or scoring.default_functions()
+        select = select or selection.default_functions()
+        mutate = mutate or mutator.default_functions()
+        reproduce = reproduce or reproduction.default_functions()
+        create = create or creation.default_functions()
         self._score = score if callable(score) else tuple(score)
         self._select = select if callable(select) else tuple(select)
         self._mutate = mutate if callable(mutate) else tuple(mutate)
@@ -60,6 +57,50 @@ class Configuration:
     @property
     def create(self) -> callable:
         return self._create if callable(self._create) else random.choice(self._create)
+
+    def __str__(self):
+        out = ''
+        for funcs, module, name in ((self._score, scoring, 'SCORE'),
+                                    (self._select, selection, 'SELECT'),
+                                    (self._mutate, mutator, 'MUTATE'),
+                                    (self._reproduce, reproduction, 'REPRODUCE'),
+                                    (self._create, creation, 'CREATE')):
+            print(funcs, module, name)
+            if not callable(funcs) and len(funcs) == 1:
+                funcs = funcs[0]
+            if callable(funcs):
+                code = utils.key_of(funcs, module.named_functions(), '')
+                out += '{}: {} {}\n'.format(name, utils.pretty_func(funcs), code)
+            else:
+                out += '{}:\n'.format(name)
+                for func in funcs:
+                    code = utils.key_of(func, module.named_functions(), '')
+                    out += '\t{} {}\n'.format(utils.pretty_func(func), code)
+        return out
+
+
+
+    @staticmethod
+    def from_codes(score:str=None, select:str=None, mutate:str=None,
+                   reproduce:str=None, create:str=None):
+        """Create configuration from given codes of named functions.
+
+        if a code is None, the default will be used
+
+        """
+        score = (scoring.default_functions() if score is None
+                 else scoring.named_functions(score))
+        select = (selection.default_functions() if select is None
+                  else selection.named_functions(select))
+        mutate = (mutator.default_functions() if mutate is None
+                  else mutator.named_functions(mutate))
+        create = (creation.default_functions() if create is None
+                  else creation.named_functions(create))
+        reproduce = (reproduction.default_functions() if reproduce is None
+                     else reproduction.named_functions(reproduce))
+        return Configuration(score=score, select=select, mutate=mutate,
+                             reproduce=reproduce, create=create)
+
 
 
     @staticmethod
