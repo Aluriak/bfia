403cd016d0efa6b455f494048142976e618da218
Aluriak <lucas.bourneuf@laposte.net>
2016-12-11 00:59:48 +0100

    case: doc + read-only attributes

--- case.py
+++ case.py
@@ -9,12 +9,22 @@ class Case:
     A scoring function can, with a unit and a case, provides a score.
     See test_case.py for API usage examples.
 
+    stdin can be either a string or a callable.
+    expected can be either a string (could contains the substring "{stdin}"),
+    or a callable (stdin value -> expected string).
+
     """
     def __init__(self, input:str or callable, expected:str or callable):
-        self.callable_input = callable(input)
-        self.callable_output = callable(expected)
-        self._input = input
-        self._expected = expected
+        self._input = input if callable(input) else str(input)
+        self._expected = expected if callable(expected) else str(expected)
+
+    @property
+    def callable_input(self):
+        return callable(self._input)
+
+    @property
+    def callable_output(self):
+        return callable(self._expected)
 
     def __iter__(self):
         if self.callable_input:
