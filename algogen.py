28a0e9886d3f05116c31eb19260cb44c0551a6cf
Aluriak <lucas.bourneuf@laposte.net>
2016-12-11 01:01:29 +0100

    algogen: one test instance per step + show success in stdout

--- algogen.py
+++ algogen.py
@@ -35,15 +35,19 @@ def step(pop, case, pop_size:int, score:callable,
     if step_number is not None:
         print('# {}'.format(step_number))
 
+    stdin, expected = case
+
     with Pool(processes=MULTIPROC_PROCESSES, maxtasksperchild=MULTIPROC_TASK_PER_CHILD) as p:
-        scored_pop = dict(zip(pop, p.starmap(score, zip(pop, itertools.repeat(case)))))
+        scored_pop = dict(zip(pop, p.starmap(score, zip(pop, itertools.repeat((stdin, expected))))))
 
     best_unit = max(pop, key=lambda u: scored_pop[u].score)
     best_result = scored_pop[best_unit]
     print('SCORES:', sorted(tuple(set(round(r.score, 3) for u, r in scored_pop.items())), reverse=True))
     proportions = Counter(r.score for r in scored_pop.values())
     print('PROPS :', proportions.most_common(MAX_PRINTED_PROPS))
-    print('OF', len(scored_pop), 'BEST:', round(best_result.score, 3), '\tOUTPUTS:', '"' + best_result.found + '"', '\t(expect {})'.format(best_result.expected))
+    print('OF', len(scored_pop), 'BEST:', round(best_result.score, 3),
+          '\tOUTPUTS:', '"' + best_result.found + '"', '\t(expect {})'.format(best_result.expected),
+          ('[SUCCESS]' if best_result.found == best_result.expected else ''))
     print('SOURCE:', best_unit.source)
 
     selected = select(scored_pop)
