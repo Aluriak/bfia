d0df728c13aa681ade3eb69b18c4b26e60e0144b
Aluriak <lucas.bourneuf@laposte.net>
2016-12-11 01:00:37 +0100

    test: case test do more tests

--- test_case.py
+++ test_case.py
@@ -4,22 +4,31 @@ import random
 from case import Case
 
 
+def build_random_id():
+    return ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(10))
+
+
 def test_case_basic_api():
 
     # hi !
     c = Case('', 'hi !')
     assert tuple(c) == ('', 'hi !')
+    assert not c.callable_input
+    assert not c.callable_output
 
     # hello <input> !
-    c = Case((lambda: ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(10))),
-             'hello {stdin} !')
+    c = Case(build_random_id, 'hello {stdin} !')
+    assert not c.callable_input
+    assert not c.callable_output
     stdin, stdout = c
     assert stdout.startswith('hello ')
     assert stdin == stdout[len('hello '):-2]
 
     # hello <tupni> !
-    c = Case((lambda: ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(3))),
+    c = Case(build_random_id,
              (lambda stdin: 'hello {} !'.format(''.join(reversed(stdin)))))
+    assert c.callable_input
+    assert c.callable_output
     stdin, stdout = c
     assert stdout.startswith('hello ')
     assert stdin == ''.join(reversed(stdout[len('hello '):-2]))
