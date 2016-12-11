1f10e201b2c47ac1988012113a370124ec683d61
Aluriak <lucas.bourneuf@laposte.net>
2016-12-11 01:00:14 +0100

    creation: fix interface + 2 new creation methods

--- creation.py
+++ creation.py
@@ -17,10 +17,12 @@ from utils import named_functions_interface_decorator
 
 
 @named_functions_interface_decorator
-def named_functions(name:str=None) -> dict:
+def named_functions() -> dict:
     """Return creation functions"""
     return {
         'MOD': memory_oriented_diversity,
+        'INP': stdin_printer,
+        'AME': all_methods_equally,
     }
     return funcs
 
@@ -35,6 +37,16 @@ def anonymous_functions() -> tuple:
     return ()
 
 
+def stdin_printer(pop_size:int):
+    """Return pop_size new Unit instance. Source code is oriented toward
+    printing of stdin with workarounds.
+
+    """
+    source = '+++.>,[.,]>+++.'
+    chrom_size = 10
+    return tuple(Unit(source, chrom_size) for _ in range(pop_size))
+
+
 def memory_oriented_diversity(pop_size:int):
     """Return pop_size new Unit instance. Source code is oriented toward
     movement and memory management, with less brackets and IO
@@ -42,3 +54,22 @@ def memory_oriented_diversity(pop_size:int):
     """
     specs = range(30, 40), range(3, 20), '<<<>>>++--[],..'
     return tuple(Unit.from_spec(*specs) for _ in range(pop_size))
+
+
+def all_methods_equally(pop_size:int):
+    """Return pop_size new Unit instance, using all the others
+    creation methods.
+
+    """
+    methods = frozenset(
+        method
+        for method in set(named_functions().values()) | set(anonymous_functions())
+        if method is not all_methods_equally
+    )
+    unit_per_method = pop_size // len(methods)
+    remaining = pop_size % len(methods)
+    for method in methods:
+        yield from method(unit_per_method)
+    # choose one for each remaining unit to produce
+    for _ in range(remaining):
+        yield from random.choice(methods)(1)
