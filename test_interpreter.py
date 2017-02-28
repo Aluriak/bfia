6e6e10eb52f144afb41867aa3a2355653ab0adcd
Aluriak <lucas.bourneuf@laposte.net>
2017-02-28 01:05:55 +0100

    UT: interpreter behavior

---
+++ test_interpreter.py
@@ -0,0 +1,25 @@
+
+
+import pytest
+from interpreter import load_interpreter
+
+
+@pytest.fixture
+def interpret():
+    return load_interpreter().inline
+
+
+def test_basic_ops(interpret):
+    for x in range(2, 127):
+        assert ord(interpret(',.', input=chr(x))) == x
+        assert ord(interpret(',+.', input=chr(x))) == x + 1
+        assert ord(interpret(',-.', input=chr(x))) == x - 1
+
+
+def test_basic_loop(interpret):
+    for x in range(1, 127):
+        assert ord(interpret(',[-]+.', input=chr(x))) == 1
+
+
+def test_basic_move(interpret):
+    assert ord(interpret(',>+.', input=chr(42))) == 1
