a689fafae99c9bdafafc6258c8058b2dccaac1ad
Aluriak <lucas.bourneuf@laposte.net>
2017-09-28 00:30:10 +0200

    reorganize main better, with more cases to show

--- __main__.py
+++ __main__.py
@@ -4,34 +4,119 @@ import random
 from mmh import MMH
 from case import Case
 from config import Configuration
-from statistics import Saver
+from statistics import Saver, ScoreSaver
 
 
 # simulation
 POP_SIZE = 400
 
 
-def main(saver:Saver=None):
-    # case = Case('', 'hi !')
+def run_interpreter_testing(_:Saver):
+    """Just show that given code do not works except in a few cases
+    for diving by 2"""
+    def test_interpreter(source, stdin=''):
+        from interpreter import load_interpreter
+        interp = load_interpreter()
+        return interp.inline(source, input=stdin)
+
+
+    test = lambda x: ord(test_interpreter(
+        '++++++-++++++++++++++++++[+++++<<+++++++++++++++++<+++++++.',
+        chr(x)
+    ))
+    for x in range(64, 128):
+        found, expected = test(x), x // 2
+        print(x, found, expected, 'OK' if found == expected else '')
+
+
+def run_simple_cases(saver:Saver):
     letters = tuple(chr(c) for c in range(32, 128))
-    case = Case(lambda: random.choice(letters), 'hi {stdin} !')
-    config = Configuration.from_codes(score='IOCBM', select='PLDD', mutate='ALL',
-                                      reproduce='SCP', create='AME', step='DIV')
+    numbers = tuple(chr(c) for c in range(64, 128))
+    CASES = {
+        'hi !': Case('', 'hi !'),
+        'hi {input} !': Case(lambda: random.choice(letters), 'hi {stdin} !'),
+        'division by 2': Case(lambda: random.choice(numbers), lambda x: chr(ord(x) // 2)),
+    }
+    # build the motif
+    config = Configuration.recipe_best_solution_so_far()
+    config = Configuration()
+    for case_name, case in CASES.items():
+        print('#'*80)
+        print('#', case_name)
+        mmh = MMH(case, pop_size=POP_SIZE, config=config)
+        for pops in mmh.corun():
+            assert len(pops) == 1  # currently multipop is not implemented
+            pop = pops[0]
+            scores = tuple(v.score for v in pop.values())
+            saver.save([mmh.current_step, max(scores)])
+        print('#'*80)
+        print()
+
+
+MOTIF = r"""
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////\\\\\/////
+"""
+
+
+
+def run_test_motif(saver:Saver):
+    case = Case('/\\\n', MOTIF)
+
+    config = Configuration.from_codes(score='IOC', select='PLDD', mutate='ALL',
+                                      reproduce='SCP', create='AME', step='DIV',
+                                     )# manipulator='DAC')
+    config = Configuration()
     # config = Configuration.recipe_best_solution_so_far()
     mmh = MMH(case, pop_size=POP_SIZE, config=config)
-    while True:
-        next(mmh.run())
+    for pops in mmh.corun():
+        assert len(pops) == 1  # currently multipop is not implemented
+        pop = pops[0]
+        scores = tuple(v.score for v in pop.values())
+        for score in scores:
+            saver.save([score, mmh.current_step])
+
+
+def main(saver:Saver=None):
+    methods = (
+        # ((), run_interpreter_testing),
+        (('step', 'max_score'), run_simple_cases),
+        # (('score', 'step'), run_test_motif),
+    )
+    for fields, method in methods:
+        with Saver(fields=fields) as saver:
+            try:
+                method(saver)
+            except KeyboardInterrupt:
+                pass
+        if fields:
+            saver.commit()
+        print('STOPPED')
+        if fields:
+            saver.plot()
+
 
 if __name__ == "__main__":
     main()
-    exit()
-    with Saver(('scores', 'max_score', 'min_score')) as saver:
-        try:
-            main(saver)
-        except KeyboardInterrupt:
-            pass
-    saver.commit()
-    print('STOPPED')
-    files = Saver.data_files()
-    print('FILES:', files)
-    Saver.plot(max(files))
