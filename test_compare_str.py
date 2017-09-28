f515b4e426493f34fa0bce53c59830db44d85535
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-09-28 16:00:30 +0200

    test: divide two tests into two functions

--- test_compare_str.py
+++ test_compare_str.py
@@ -5,10 +5,13 @@ import utils
 from scoring import compare_str_c, compare_str_py, UINT8_MAX
 
 
-def test_compare_str():
-    for method in (compare_str_c, compare_str_py):
-        assert method('a', 'b') == 1
-        assert method('a', 'bc') == abs(ord('a') - ord('b')) + abs(UINT8_MAX)
+def test_compare_str_c():
+    assert compare_str_c('a', 'b') == 1
+    assert compare_str_c('a', 'bc') == abs(ord('a') - ord('b')) + abs(UINT8_MAX)
+
+def test_compare_str_py():
+    assert compare_str_py('a', 'b') == 1
+    assert compare_str_py('a', 'bc') == abs(ord('a') - ord('b')) + abs(UINT8_MAX)
 
 def test_utils_reversed_dict():
     dct = {1: 2, 3: 4, 5: 6, 7: 2}
